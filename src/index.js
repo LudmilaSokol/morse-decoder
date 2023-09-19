a772652d12b0fbf297c511ad11292124c3d4bdea
LudmilaSokol <sluda999@yandex.ru>
2023-09-19 15:40:33 +0500

    init: decision morse-decoder

--- src/index.js
+++ src/index.js
@@ -39,6 +39,33 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+    let result = '';
+    let binaryLetter;
+    let b, letter;
+    let modifedExpr = expr;
+    let codeMorze = '';
+    do {
+        binaryLetter = modifedExpr.slice(0, 10);
+        if (binaryLetter !== '**********'){
+            codeMorze = '';
+            do {
+                b=binaryLetter.slice(0, 2);
+                binaryLetter = binaryLetter.slice (2);
+                if (b === '11') {
+                    codeMorze = codeMorze + '-';
+                } else if (b === '10') {
+                    codeMorze = codeMorze + '.';
+                }
+            } while (binaryLetter >= 2)
+            letter = MORSE_TABLE[codeMorze];
+            result = result + letter;
+        } else {
+            result = result + ' ';
+        }
+        modifedExpr = modifedExpr.slice(10);
+    } while (modifedExpr.length >= 10);
+
+    return result;
 }
 
 module.exports = {
